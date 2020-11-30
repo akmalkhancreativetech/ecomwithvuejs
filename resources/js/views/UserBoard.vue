dd440f99adaa6da6e53dd5252f57cb7c9e17a256
Akmal Khan <khan.akmal542@gmail.com>
2020-11-30 08:44:10 +0500

    Till Creating the user dashboard Completed From Home

--- resources/js/views/UserBoard.vue
+++ resources/js/views/UserBoard.vue
@@ -1,13 +1,45 @@
 <template>
-    
+    <div>
+        <div class="container-fluid hero-section d-flex align-content-center justify-content-center flex-wrap ml-auto">
+            <h2 class="title">All your orders</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12"><br>
+                    <div class="row">
+                        <div class="col-md-4 product-box" v-for="(order, index) in orders" @key="index">
+                            <img :src="order.product.image" :alt="order.product.name">
+                            <h5><span v-html="order.product.name"></span><br>
+                                <span class="text-small text-muted">$ {{order.product.price}}</span>
+                            </h5>
+                            <hr>
+                            <span class="text-small text-muted">Quantity: {{order.quantity}}
+                                <span class="float-right">{{order.is_delivered == 1 ? "shipped!" : "not shipped"}}</span>
+                            </span><br><br>
+                            <p><strong>Delivery address:</strong> <br>{{order.address}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "register"
+        data() {
+            return { user: null, orders: [] }
+        },
+        beforeMount(){
+            this.user = JSON.parse(localStorage.getItem('bigStore.jwt'))
+            axios.defaults.headers.common['Content-Type'] = 'application/json'
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('bigStore.jwt')
+            axios.get(`api/users/${this.user.id}/orders`).then(response => this.orders = response.data)
+        }
     }
 </script>
 
 <style scoped>
-
+    .product-box { border: 1px solid #cccccc; padding: 10px 15px; }
+    .hero-section { background: #ababab; height: 20vh; align-items: center; margin-bottom: 20px; margin-top: -20px; }
 </style>
