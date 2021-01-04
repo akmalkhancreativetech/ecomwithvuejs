8812fbb2cf0c6e839c882c0276c0258b246bfcc5
Akmal Khan <akmalkhancreativetechsol@gmail.com>
2021-01-04 15:43:24 +0500

    Changes in Admin.vue

--- resources/js/views/Admin.vue
+++ resources/js/views/Admin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="container-fluid hero-section d-flex align-content-center justify-content-center flex-wrap ml-auto">
-            
+
         </div>
     </div>
 </template>
